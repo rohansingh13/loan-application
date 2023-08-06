80e75711acd369bf45acfe58005ae8135f189e1b
Rohan Singh <rsi@Nova.local>
2023-08-06 16:10:21 +0200

    Initial commit

---
+++ loan-application-backend/src/test/java/com/example/loanapplicationbackend/LoanApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanapplicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
