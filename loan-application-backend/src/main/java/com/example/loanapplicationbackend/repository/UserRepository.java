14f33bc2b999fed8430b512ffe0992c19d12343e
Rohan Singh <rsi@Nova.local>
2023-08-07 12:02:45 +0200

    Implementing authentication and authorization mechanism in backend using JWT spring security

---
+++ loan-application-backend/src/main/java/com/example/loanapplicationbackend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.loanapplicationbackend.repository;
+
+import com.example.loanapplicationbackend.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+    public User findUserByEmail(String email){
+        User user = new User(email,"123456");
+        user.setFirstName("FirstName");
+        user.setLastName("LastName");
+        return user;
+    }
+}
