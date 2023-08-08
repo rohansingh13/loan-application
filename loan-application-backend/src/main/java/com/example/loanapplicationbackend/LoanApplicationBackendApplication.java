abf08e4757f0851e2105361de6dfdb040042318c
Rohan Singh <rsi@Nova.local>
2023-08-08 19:00:07 +0200

    Changes related to api documentation using swagger

--- loan-application-backend/src/main/java/com/example/loanapplicationbackend/LoanApplicationBackendApplication.java
+++ loan-application-backend/src/main/java/com/example/loanapplicationbackend/LoanApplicationBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.loanapplicationbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class LoanApplicationBackendApplication {
 
 	public static void main(String[] args) {
