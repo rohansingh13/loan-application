8a1a06859848d690194a216751c3bb0dfbb295af
Rohan Singh <singh.rohan13@gmail.com>
2023-09-27 20:55:10 +0200

    Added EntityScan in SpringBoot application

--- loan-application-backend/src/main/java/com/example/loanapplicationbackend/LoanApplicationBackendApplication.java
+++ loan-application-backend/src/main/java/com/example/loanapplicationbackend/LoanApplicationBackendApplication.java
@@ -2,9 +2,11 @@ package com.example.loanapplicationbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EntityScan
 @EnableSwagger2
 public class LoanApplicationBackendApplication {
 
