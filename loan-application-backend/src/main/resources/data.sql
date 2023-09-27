d9333501530f9eb86aa15d6820412d6d1c26f98c
Rohan Singh <rsi@Nova.local>
2023-09-27 20:26:40 +0200

    Switch database configuration from H2 to MySQL

--- loan-application-backend/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO customer(full_name,loan_amount,equity_amount,salary_amount) values('Johny Test',30000,20450,500000);
-
-INSERT INTO customer(full_name,loan_amount,equity_amount,salary_amount) values('Jane Test',50000,30458,800000);
-
-INSERT INTO customer(full_name,loan_amount,equity_amount,salary_amount) values('Sachin Test',80000,50457,900000);
